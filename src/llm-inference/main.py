a8e1f0a43323277a5c9ebc6e3c3b027f943d1a07
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2024-10-08 15:59:19 +0800

    feat: llm inference example

---
+++ src/llm-inference/main.py
@@ -0,0 +1,36 @@
+from torch.profiler import profile, ProfilerActivity
+from transformers import AutoTokenizer, TextStreamer, AutoModelForCausalLM
+from threading import Thread
+import intel_npu_acceleration_library
+from intel_npu_acceleration_library.compiler import CompilerConfig
+import torch
+import time
+import sys
+
+model_id = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+
+model = AutoModelForCausalLM.from_pretrained(model_id, use_cache=True).eval()
+tokenizer = AutoTokenizer.from_pretrained(
+    model_id, use_default_system_prompt=True)
+tokenizer.pad_token_id = tokenizer.eos_token_id
+streamer = TextStreamer(tokenizer, skip_special_tokens=True)
+
+
+print("Compile model for the NPU")
+config = CompilerConfig(dtype=torch.float64)
+model = intel_npu_acceleration_library.compile(model, config)
+
+query = "What is the meaning of life?"
+prefix = tokenizer(query, return_tensors="pt")["input_ids"]
+
+
+generation_kwargs = dict(
+    input_ids=prefix,
+    streamer=streamer,
+    do_sample=True,
+    top_k=50,
+    top_p=0.9,
+)
+
+print("Run inference")
+_ = model.generate(**generation_kwargs)
