6c8b9207bd8a471bde6f7668dcfe385fe4850c10
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2024-10-08 14:50:44 +0800

    feat: run a llama model on the NPU

---
+++ quick-start/main.py
@@ -0,0 +1,27 @@
+# Source: https://intel.github.io/intel-npu-acceleration-library/index.html#run-a-llama-model-on-the-npu
+
+from transformers import AutoTokenizer, TextStreamer
+from intel_npu_acceleration_library import NPUModelForCausalLM
+import torch
+
+model_id = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+
+model = NPUModelForCausalLM.from_pretrained(model_id, use_cache=True, dtype=torch.int8).eval()
+tokenizer = AutoTokenizer.from_pretrained(model_id, use_default_system_prompt=True)
+tokenizer.pad_token_id = tokenizer.eos_token_id
+streamer = TextStreamer(tokenizer, skip_special_tokens=True)
+
+query = input("Ask something: ")
+prefix = tokenizer(query, return_tensors="pt")["input_ids"]
+
+generation_kwargs = dict(
+   input_ids=prefix,
+   streamer=streamer,
+   do_sample=True,
+   top_k=50,
+   top_p=0.9,
+   max_new_tokens=512,
+)
+
+print("Run inference")
+_ = model.generate(**generation_kwargs)
